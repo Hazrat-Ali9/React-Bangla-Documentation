5325e3da088c3170467170f1af020f1b6ab2e8a5
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-06-04 12:11:39 +0600

    Donate   😇

--- components/Donate.tsx
+++ components/Donate.tsx
@@ -1,19 +1,19 @@
 import React from "react";
 import styles from "./donate.module.css";
-
+// Donate 
 const Donate: React.FC = () => {
   return (
     <div className={styles.donateContainer}>
       <div className={styles.donateCard}>
         <h2 className={styles.donateTitle}>
-          ☕ আপনার একটু ছোট ডোনেশন দিয়ে অনেক কিছু সম্ভব!
+          
         </h2>
         <p className={styles.donateText}>
-          আমি React ডকুমেন্টেশন বাংলা ভাষায় অনুবাদ করতেছি বিগিনারদের জন্য।{" "}
+          {" "}
           <strong className={styles.highlight}>
-            আমি প্রতিনিয়ত ব্যবহারযোগ্য নতুন নতুন ফিচার ও কোড
+            
           </strong>{" "}
-          আপনাদের সাথে শেয়ার করি।
+        
         </p>
         <p className={styles.donateText}>
           {" "}
